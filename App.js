65db2d92abea5b7b2f6e2696440703aeb5ff5698
Richard Magnusson <richardmagnusson@Richards-MacBook-Pro.local>
2018-11-30 14:30:46 +0100

    added tabs for different pages in app

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
 // import { BarcodeScan } from '../Scanner'
 import BarcodeScannerExample from './Scanner'
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Insights!</Text>
+      </View>
+    );
+  }
+}
+
+class ScannerScreen extends React.Component {
   render () {
     return (
       <SafeAreaView style={styles.safeArea}>
@@ -22,6 +34,24 @@ export default class App extends React.Component {
     )
   }
 }
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Insights: HomeScreen,
+  Scanner: ScannerScreen,
+  Settings: SettingsScreen
+});
+
+export default createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   safeArea: {
