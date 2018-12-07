b29ba449d37ebf41367aba9b4c9b88a3d25fb22b
Joel Ottosson <joel.ottosson.95@gmail.com>
2018-12-07 10:23:21 +0100

    Working fetch

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
-import { getMoviesFromApi } from './tools/dabasGET'
+//import getFoodData, { getMoviesFromApi } from './tools/dabasGET'
 
 // import { BarcodeScan } from '../Scanner'
 import BarcodeScannerExample from './Scanner'
+import DabasGET from './tools/dabasGET'
 
 class HomeScreen extends React.Component {
-  render() {
+  render () {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Insights!</Text>
@@ -20,24 +21,26 @@ class ScannerScreen extends React.Component {
   render () {
     return (
       <SafeAreaView style={styles.safeArea}>
-      <View>
-
-        <View style = {styles.scanner}>
-          <BarcodeScannerExample/>
-        </View>
-
         <View>
-          <Text style = {styles.text}>{console.log(function () => this.getMoviesFromApi)}Barcode Scanner</Text>
+
+          <View style={styles.scanner}>
+            <BarcodeScannerExample />
+          </View>
+
+          <View>
+            <Text style={styles.text}>Barcode Scanner</Text>
+          </View>
+
+          <View><DabasGET></DabasGET></View>
+
         </View>
-        
-      </View>
       </SafeAreaView>
     )
   }
 }
 
 class SettingsScreen extends React.Component {
-  render() {
+  render () {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Settings!</Text>
--- tools/dabasGET.js
+++ tools/dabasGET.js
@@ -1,15 +1,31 @@
-import fetch from 'react-native'
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 
-async function getMoviesFromApi() {
-    try {
-      let response = await fetch(
-        'http://api.dabas.com/DABASService/V2/article/gtin/05707381014040/JSON?apikey=e7886b21-bd2f-40e0-b7a1-625967c2679b',
-      );
-      let responseJson = await response.json();
-      return responseJson.Artikelbenamning;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  export default { getMoviesFromApi }
+class DabasGet extends Component {
+   state = {
+      data: ''
+   }
+   componentDidMount = () => {
+      fetch('http://api.dabas.com/DABASService/V2/article/gtin/05707381014040/JSON?apikey=e7886b21-bd2f-40e0-b7a1-625967c2679b', {
+         method: 'GET'
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+         console.log(responseJson);
+         this.setState({
+            data: responseJson
+         })
+      })
+      .catch((error) => {
+         console.error(error);
+      });
+   }
+   render() {
+      return (
+            <Text>
+               {this.state.data.Artikelbenamning}
+            </Text>
+      )
+   }
+}
+export default DabasGet
