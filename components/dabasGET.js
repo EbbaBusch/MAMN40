3a372e32cfc2ac88555387c6942565a169fed710
EbbaBusch <ebba.linnea.busch@gmail.com>
2018-12-12 15:05:11 +0100

    get test

--- components/dabasGET.js
+++ components/dabasGET.js
@@ -5,26 +5,28 @@ export default class DabasGet extends Component {
   state = {
     data: ''
   }
-  componentDidMount = () => {
-    fetch('http://api.dabas.com/DABASService/V2/article/gtin/' + this.props.artNum + '/JSON?apikey=e7886b21-bd2f-40e0-b7a1-625967c2679b', {
+  get = () => {
+    
+  }
+  render() {
+
+    fetch('http://api.dabas.com/DABASService/V2/article/gtin/0' + this.props.artNum + '/JSON?apikey=e7886b21-bd2f-40e0-b7a1-625967c2679b', {
       method: 'GET'
     })
       .then((response) => response.json())
       .then((responseJson) => {
         console.log(responseJson);
-        this.setState({
-          data: responseJson
-        })
+        // this.setState({
+        //   data: responseJson
+        // })
+        return (
+          <Text>
+            {this.state.data.Artikelbenamning}
+          </Text>
+        )
       })
       .catch((error) => {
         console.error(error);
       });
-  }
-  render() {
-    return (
-      <Text>
-        {this.state.data.Artikelbenamning}
-      </Text>
-    )
   }
 }
