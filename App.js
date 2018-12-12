48c35fba04b44dab2c448ed9fcb66d24a32569ad
richardmagnusson <richard.pe.magnusson@gmail.com>
2018-12-12 17:33:30 +0100

    added fake data to insights screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from 'react-native'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
 //import getFoodData, { getMoviesFromApi } from './tools/dabasGET'
 
@@ -10,9 +10,26 @@ import DabasGET from './components/dabasGET'
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Insights!</Text>
-      </View>
+      <SafeAreaView style={styles.safeArea}>
+      <ScrollView style={{alignContent: 'center'}}>
+        <Image
+          source={require('./assets/tiles/tile_1.png')}
+          style={{alignSelf: 'stretch', width: 356, height: 193, marginTop: 10, marginLeft: 10, marginRight: 10}}
+        />
+        <Image
+          source={require('./assets/tiles/tile_2.png')}
+          style={{alignSelf: 'stretch', width: 356, height: 167, marginTop: 10, marginLeft: 10, marginRight: 10}}
+        />
+        <Image
+          source={require('./assets/tiles/tile_4.png')}
+          style={{alignSelf: 'stretch', width: 356, height: 167, marginTop: 10, marginLeft: 10, marginRight: 10}}
+        />
+        <Image
+          source={require('./assets/tiles/tile_3.png')}
+          style={{alignSelf: 'stretch', width: 356, height: 265, marginTop: 10, marginLeft: 10, marginRight: 10}}
+        />
+      </ScrollView>
+      </SafeAreaView>
     );
   }
 }
